898ea49784d11ddc1cdb40f138e0a4c1ee3654b4
pri65 <priyankasoniii65@gmail.com>
2023-01-16 22:11:32 +0530

    render chat feed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       projectID='15161ddc-27f9-42f8-b140-208f646b85cb'
       userName='priyanka'
       userSecret='1234'
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
     </div>
   );
